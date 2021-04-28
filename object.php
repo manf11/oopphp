e29f2d45484c554d930dbb7612accf7e0452f945
manf11 <mohahlisnoorfuad1@gmail.com>
2021-04-28 23:43:10 +0700

    tutor oopphp di object udh sesuai sama yg di wpu

--- object.php
+++ object.php
@@ -8,10 +8,10 @@ class Produk{
 
     public function __construct($judul = "judul",$penulis = "penulis",$penerbit = "penerbit",$harga =0)
     {
-        $this->$judul = $judul;
-        $this->$penulis = $penulis;
-        $this->$penerbit = $penerbit;
-        $this->$harga = $harga;
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
     }
 
     // public function sayHello(){
